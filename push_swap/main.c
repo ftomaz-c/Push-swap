2281002422309589fb2acc5eee5f4f1534515cdd
ftomaz-c <fabiocastim@gmail.com>
2023-08-17 21:09:09 +0100

    17Ago2109

--- push_swap/main.c
+++ push_swap/main.c
@@ -6,68 +6,99 @@
 /*   By: ftomaz-c <ftomaz-c@student.42lisboa.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/16 13:40:36 by ftomaz-c          #+#    #+#             */
-/*   Updated: 2023/08/16 16:18:46 by ftomaz-c         ###   ########.fr       */
+/*   Updated: 2023/08/17 21:07:17 by ftomaz-c         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "../libft.h"
 #include "push_swap.h"
 
-// argv, can be passed as 2 arguments with the use of "";
-// stack init
-// swap
-// 	push swap
+void	free_nodes(t_list *a) {
 
-void	free_nodes(stack_node **stack, int index) {}
-
-void	stack_init(stack_node **a, t_list **new_argv) {
-
-	int		nbr;
-	int		index;
 	t_list	*current;
+	t_list	*temp;
 
-	index = 0;
-	current = *new_argv;
-	while(current) {
-		nbr = ft_atoi(current->content);
-		a[index] = malloc(sizeof(stack_node));
-		if (!a[index]) {
-			free_nodes(a, index);
-			return;
-		}
-		a[index]->data = malloc(sizeof(int));
-		if (!a[index]->data) {
-			free_nodes(a, index);
-			return;
-		}
-		a[index]->data = nbr;
-		a[index]->index = index;
-		a[index]->next = NULL;
-		if (index == 0)
-			a[index]->prev = NULL;
-		else
-			a[index]->prev = a[index - 1];
-		index++;
+	current = a;
+	while (current != NULL) {
+		temp = current;
 		current = current->next;
+		free (temp);
 	}
 }
 
-int	error_arg_type(t_list **argv) {}
+t_list	*create_node(int data, int index)
+{
+	t_list	*temp;
+
+	temp = malloc(sizeof(t_list));
+	if (!temp)
+		return (NULL);
+	temp->data = data;
+	temp->index = index;
+	temp->next = NULL;
+	temp->prev = NULL;
+	return (temp);
+}
+
+void	stack_init(t_list **head_a, t_list **tail_a, char **argv)
+{
+	t_list	*temp;
+	int		index;
+
+	index = 1;
+	while (argv[index] != NULL)
+	{
+		temp = create_node(ft_atoi(argv[index]), index);
+		if (!temp)
+		{
+			free_nodes(*head_a);
+			return;
+		}
+		if (*head_a == NULL)
+			*head_a = temp;
+		if (*tail_a == NULL)
+			*tail_a = temp;
+		else
+		{
+			temp->prev = *tail_a;
+			(*tail_a)->next = temp;
+			*tail_a = temp;
+		}
+		index++;
+	}
+}
+
+/* int	error_arg_type(char **argv) {} */
 
 int	main(int argc, char **argv)
 {
-	stack_node	*a;
-	stack_node	*b;
-	t_list		**new_argv;
+	t_list	*head_a;
+	t_list	*tail_a;
+	t_list	*a;
+	// t_list	*head_b;
+	// t_list	*tail_b;
 
-	a = NULL;
-	b = NULL;
-	if(argc == 1 || (argc == 2 && !argv[1][0]))
+	head_a = NULL;
+	tail_a = NULL;
+	// head_b = NULL;
+	// tail_b = NULL;
+	if (argc == 1 || (argc == 2 && !argv[1][0]))
 		return 0;
 	else if (argc == 2)
-		new_argv = ft_split(argv[1], ' ');
-	if (!error_arg_type(new_argv)) //error handling for the arguments
-		stack_init(&a, new_argv);
-	else
-		return(ft_printf("Error\n"));
+		argv = ft_split(argv[1], ' ');
+	stack_init(&head_a, &tail_a, argv);
+	if (argc == 2)
+	{
+		free_stash(argv, count_words(argv[1], ' '));
+		free(argv);
+	}
+	a = head_a;
+	ft_printf("Contents of the linked list:\n");
+	while (a != NULL)
+	{
+		ft_printf("index:	%i, data:	%i\n", a->index, a->data);
+		a = a->next;
+	}
+	ft_printf("\n");
+	free_nodes(head_a);
+	return 0;
 }
--- push_swap/push_swap.h
+++ push_swap/push_swap.h
@@ -6,22 +6,23 @@
 /*   By: ftomaz-c <ftomaz-c@student.42lisboa.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/16 12:18:08 by ftomaz-c          #+#    #+#             */
-/*   Updated: 2023/08/16 14:17:40 by ftomaz-c         ###   ########.fr       */
+/*   Updated: 2023/08/17 19:08:40 by ftomaz-c         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef PUSH_SWAP_H
 # define PUSH_SWAP_H
 
-# include "libft.h"
+# include "libft/src/libft.h"
 
-typedef struct	stack_list
+typedef struct	s_list
 {
 	int				data;
 	int				index;
-	struct stack_list	*prev;
-	struct stack_list	*next;
-}		stack_node;
+	struct s_list	*prev;
+	struct s_list	*next;
+}				t_list;
 
+void	stack_init(t_list **head_a, t_list **tail_a, char **argv);
 
 #endif
